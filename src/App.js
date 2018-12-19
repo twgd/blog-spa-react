3ac91bb7445563215aad43950e9e17b65644a1cf
twgd <twgd.huang@gmail.com>
2018-12-20 04:17:46 +0800

    basic

--- src/App.js
+++ src/App.js
@@ -1,28 +1,93 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*
+    處理部落格 ( home & about )
+*/
 
-class App extends Component {
-  render() {
+import React from 'react';
+import Posts from './Posts';
+
+
+const About = () => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+        <div>
+            <div className="my-3">
+                <h2>關於我</h2>
+            </div>
+        </div>
+    )
+}
+
+const Home = () => {
+    return (
+        <div>
+            <div className="my-3">
+                <h2>首頁</h2>
+            </div>
+        </div>
+    )
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            tab: 'home'
+        }
+    }
+
+    handleTabChange(e) {
+        // 防止超連結
+        e.preventDefault();
+
+        const newTab = e.target.name;
+        
+        this.setState({
+            tab: newTab,
+        })
+        
+    }
+
+    render() {
+        const {tab} = this.state;
+
+        return (
+            <div>
+                <nav className="navbar navbar-dark bg-dark">
+                    <div className="container">
+                        <ul className="navbar-nav d-flex flex-row">
+                            <li className="nav-item mr-3">
+                                <button
+                                    className={"btn btn-outline-info " + (tab === 'home' ? 'active' : '')}
+                                    name="home"
+                                    onClick={(e) => this.handleTabChange(e)}
+                                >Home</button>
+                            </li>
+                            <li className="nav-item mr-3">
+                                <button
+                                    className={"btn btn-outline-info " + (tab === 'about' ? 'active' : '')}
+                                    name="about"
+                                    onClick={(e) => this.handleTabChange(e)}
+                                >About</button>
+                            </li>
+                            <li className="nav-item mr-3">
+                                <button
+                                    className={"btn btn-outline-info " + (tab === 'posts' ? 'active' : '')}
+                                    name="posts"
+                                    onClick={(e) => this.handleTabChange(e)}
+                                >Posts</button>
+                            </li>
+                        </ul>  
+                    </div>
+                </nav>
+
+                <div className="container">
+                    {tab === 'home' && <Home />}
+                    {tab === 'about' && <About />}
+                    {tab === 'posts' && <Posts />}
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
